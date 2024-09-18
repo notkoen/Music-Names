2e20ced46bd1679ba4a2df9f180f6c8629b5f0f2
koen <45914779+notkoen@users.noreply.github.com>
2024-09-18 02:43:14 -0700

    Add lyrics

---
+++ lyrics/ze_ffxii_salikawood_v1_8s.lua
@@ -0,0 +1,28 @@
+GM.ZELyrics = {
+	["hiroyuki sawano - kingdom-due.mp3"] = {
+		{time = 143.4, text = "I've been waiting for the sun"},
+		{time = 146.8, text = "Know the day is gonna come"},
+		{time = 149.2, text = "When all my bleeding is all gone"},
+		{time = 152.3, text = "In our mighty kingdom"},
+		{time = 154.2, text = "I've been waiting on the wire"},
+		{time = 157.6, text = "I won't burn down to the fire"},
+		{time = 159.7, text = "Never been one to cry"},
+		{time = 161.8, text = "And I better get more"},
+		{time = 163.3, text = "Better get more with my sword"},
+
+		{time = 185.0, text = "And I saw the place that I always longed to be"},
+		{time = 190.3, text = "I was leading an army, head of the whole cavalry"},
+		{time = 195.7, text = "I could conquer any foes knowing you were there with me"},
+		{time = 201.1, text = "Flying high see the heroes we gonna be!"},
+		{time = 204.3, text = "Just wanna be"},
+
+		{time = 208.2, text = "Spirit of fire"},
+		{time = 209.6, text = "Everyone lies"},
+		{time = 210.9, text = "But you help a friend"},
+		{time = 212.9, text = "In the end we will get our due"},
+		{time = 218.9, text = "Spirit of fire"},
+		{time = 220.4, text = "Everyone lies"},
+		{time = 221.7, text = "But you help a friend"},
+		{time = 223.7, text = "In the end we will get our due"},
+	},
+}
