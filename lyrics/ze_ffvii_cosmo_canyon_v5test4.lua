fbd388e4df2d0e9a9368a03b55f33833b5f407e5
Whennya <anxorphiaa@gmail.com>
2024-10-01 07:05:38 -0700

    Create ze_ffvii_cosmo_canyon_v5test4.lua
    manually checking between ze_ffvii_cosmo_canyon_v5fix and ze_ffvii_cosmo_canyon_v5test4, they are the same files so can just copy paste

---
+++ lyrics/ze_ffvii_cosmo_canyon_v5test4.lua
@@ -0,0 +1,92 @@
+-- TODO create helper functions to repeat certain sections
+GM.ZELyrics = {
+	["ffvii_out_of_hell.mp3"] = {
+		-- intro
+		{time = 12.9, text = "Oh, oh, oh"},
+		{time = 14.9, text = "Oh, oh"},
+		{time = 16, text = "Oh, oh, oh"},
+		{time = 18, text = "Oh, oh"},
+		-- verse 1
+		{time = 18.8, text = "I've been down in the dirt, lost for so long"},
+		{time = 22.2, text = "And pushed around, beaten down"},
+		{time = 25.2, text = "Got nothing left to lose, all hope is gone"},
+		{time = 28.5, text = "Got buried deep underground"},
+		-- prechorus
+		{time = 31.8, text = "Oh, can you see me?"},
+		{time = 38.1, text = "Oh, can you hear me?"},
+		-- chorus
+		{time = 41.8, text = "Can you hear me screaming for you?"},
+		{time = 44.8, text = "I'm afraid I'm gonna die down here"},
+		{time = 47.8, text = "I can't save, I can't save myself"},
+		{time = 51.2, text = "Get me out, get me out of Hell"},
+		{time = 54.1, text = "I'm suffocating waiting for you"},
+		{time = 57.4, text = "Cause the angels don't fly down here"},
+		{time = 60.4, text = "I need you because no one else"},
+		{time = 63.5, text = "Can get me out, get me out of Hell?"},
+		-- verse 2
+		{time = 72.5, text = "I can't trust anyone, all that I got"},
+		{time = 75.7, text = "Is another scarred, broken heart"},
+		{time = 78.8, text = "I've been stabbed in the back, the bleeding won't stop"},
+		{time = 82, text = "Just another cut in the dark"},
+		-- prechorus
+		{time = 85.5, text = "Oh, can you see me?"},
+		{time = 91.8, text = "Oh, can you hear me?"},
+		-- chorus
+		{time = 95.3, text = "Can you hear me screaming for you?"},
+		{time = 98.5, text = "I'm afraid I'm gonna die down here"},
+		{time = 101.4, text = "I can't save, I can't save myself"},
+		{time = 104.8, text = "Get me out, get me out of Hell"},
+		{time = 107.8, text = "I'm suffocating waiting for you"},
+		{time = 111.1, text = "Cause the angels don't fly down here"},
+		{time = 114.1, text = "I need you because no one else"},
+		{time = 117.2, text = "Can get me out, get me out of Hell?"},
+		-- chorus
+		{time = 145.8, text = "Can you hear me screaming for you?"},
+		{time = 149, text = "I'm afraid I'm gonna die down here"},
+		{time = 152, text = "I can't save, I can't save myself"},
+		{time = 155.4, text = "Get me out, get me out of Hell"},
+		{time = 158.3, text = "I'm suffocating waiting for you"},
+		{time = 161.7, text = "Cause the angels don't fly down here"},
+		{time = 164.7, text = "I need you because no one else"},
+		{time = 167.8, text = "Can get me out, get me out of Hell?"},
+		-- outro
+		{time = 171.5, text = "Can you hear me?"},
+		{time = 174.7, text = "Can you see me?"},
+		{time = 177.8, text = "I can feel you now"},
+		{time = 180.6, text = "Get me out, get me out of Hell"},
+		{time = 183.8, text = "Can you hear me screaming for you?"},
+		{time = 187, text = "I'm afraid I'm gonna die down here"},
+		{time = 190, text = "I need you because no one else"},
+		{time = 193, text = "Can get me out, get me out of Hell?"},
+	},
+	["ffvii_propane_nightmares.mp3"] = {
+		-- intro
+		{time = 2.1, text = "In a trail of fire I know we will be free again"},
+		{time = 7.5, text = "In the end we will be one"},
+		{time = 13, text = "In a trail of fire I'll burn before you bury me"},
+		{time = 18.4, text = "Set your sights for the sun"},
+
+		{time = 46.1, text = "Mind is willing, soul remains"},
+		{time = 49, text = "This woman cannot be saved"},
+		{time = 51.6, text = "From the draw into the fire"},
+
+		{time = 57, text = "Mind is willing, soul remains"},
+		{time = 59.9, text = "This woman cannot be saved"},
+		{time = 62.7, text = "From the draw into the fire"},
+
+		{time = 65.5, text = "Anything to bring it on home"},
+		{time = 72.3, text = "Bring it on home"},
+		{time = 77.9, text = "Bring it on home"},
+		{time = 83.4, text = "Bring it on home"},
+
+		{time = 112.3, text = "Much to weak to jump yourself"},
+		{time = 115.1, text = "Heal the wounds or crack the shell"},
+		{time = 117.9, text = "Lift yourself from once below"},
+
+		{time = 123.3, text = "Much to weak to jump yourself"},
+		{time = 126.1, text = "Heal the wounds or crack the shell"},
+		{time = 128.9, text = "Lift yourself from once below"},
+
+		{time = 131.7, text = "Praise the anger bring it on home"},
+	},
+}
