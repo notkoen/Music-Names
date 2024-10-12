1a9abbb966732196f7c1885eaddaafbe0b6045a8
Whennya <anxorphiaa@gmail.com>
2024-10-12 02:43:44 -0700

    a

---
+++ lyrics/ze_ffvii_malgo_reactor_css5.lua
@@ -0,0 +1,6 @@
+GM.ZELyrics = {
+    ["pendulum_the_island_to_glory.mp3"] = {
+        {time = 0, text = "Pendulum - The Island"},
+        {time = 35.1, text = "Two Steps From Hell - To Glory"}
+    }
+}
